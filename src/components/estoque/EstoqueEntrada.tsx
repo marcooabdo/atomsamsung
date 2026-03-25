0465af5a8e271a4d5bf2980bbc25d815df6b8243
marcooabdo <marcooabdo@gmail.com>
2026-03-25 01:13:59 -0300

    Updated EstoqueEntrada.tsx

--- src/components/estoque/EstoqueEntrada.tsx
+++ src/components/estoque/EstoqueEntrada.tsx
@@ -846,7 +846,7 @@ export function EstoqueEntrada({ selectedUnidade, user: userProp }: EstoqueEntra
           'Descrição': p.descricao,
           'NCM': detMeta.ncm,
           'CFOP': detMeta.cfop,
-          'ID': p.id_sequencial || '',
+          'ID': p.id_numerico || '',
           'Qtd': 1,
           'Valor Unitário': p.valor_unitario_sem_imposto || 0,
           'Valor c/ Impostos': p.valor_com_impostos || 0,
